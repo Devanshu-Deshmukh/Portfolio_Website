45e87eb8b485245a756497711f75346d998a4a18
unknown <devanshudeshmukh04@gmail.com>
2026-04-23 21:39:00 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useDarkMode } from './hooks/useDarkMode';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { useDarkMode } from "./hooks/useDarkMode";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   const [isDark, setIsDark] = useDarkMode();
